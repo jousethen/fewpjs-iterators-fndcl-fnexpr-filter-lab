8ae9e297dc0fb408fc413541bdf0dd5a072b81fe
jousethen <jthen93@gmail.com>
2021-08-23 20:26:24 -0400

    tests pass

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution here
+function findMatching(drivers, arg) {
+  return drivers.filter(function (d) {
+    return d.toLowerCase() === arg.toLowerCase();
+  });
+}
+
+function fuzzyMatch(drivers, arg) {
+  return drivers.filter(function (d) {
+    let match = false;
+
+    for (let i = 0; arg.length - 1; i++) {
+      if (d[i] === arg[i]) {
+        match = true;
+      } else {
+        match = false;
+      }
+
+      return match;
+    }
+  });
+}
+
+function matchName(drivers, arg) {
+  return drivers.filter(function (d) {
+    return d.name === arg;
+  })
+}
+
